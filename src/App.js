f40e7652ebe944071cb9cd052754a0f0bd702861
IyaadC <161286521+IyaadC@users.noreply.github.com>
2026-03-03 17:35:15 +0530

    Added background layer, main menu buttons, desired custom font, VHS style overlay

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import '../public/background.png';
-
+//import '../public/background.png';
+import Button from '../src/Components/Button';
 
 function App() {
+  function handleClick(){
+    console.log("Button is clicked !")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style = {{backgroundImage: "url('/background.png')"}}>
+    
+        <div className = "content-box">
+        <img className='scanline-overlay' src='/vhs_scanline.png' alt="CRT Scanline Overlay" />
+       
+        <Button className="menu-Button" text=">> Home" onClick={handleClick}/>
+        <br />   
+        <Button className="menu-Button" text=">> Education" onClick={handleClick}/>
+        <br />
+        <Button className="menu-Button" text=">> Experience" onClick={handleClick}/>
+        <br />
+        <Button className="menu-Button" text=">> Projects" onClick={handleClick}/>
+        <br />
+        <Button className="menu-Button" text=">> Contact" onClick={handleClick}/>
+        <br />
         
-        
+        </div>
+        {/*<img src= "/background.png" alt = "Site's Background" /> */}
+      
       </header>
     </div>
   );
